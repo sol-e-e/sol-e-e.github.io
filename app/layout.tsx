6d630713698865e5b102a84601629e023fc3825a
sol-e-e <iamsol.sollee@gmail.com>
2024-12-23 13:29:44 +0900

    add meta google site verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -68,6 +68,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png" />
       <link rel="manifest" href="/static/favicons/site.webmanifest" />
       <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5" />
+      <meta name="google-site-verification" content="pJTzH2P1xrJRD7YKaF4xIDHhgTnyTBFkfhSk1BSo6eI" />
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
