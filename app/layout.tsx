01603169f9d40a32e33060346a4fdc1145846d17
sol-e-e <iamsol.sollee@gmail.com>
2025-08-22 19:48:42 +0900

    ⬆️ upgrade dependecies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
-import 'css/tailwind.css'
+import '@/css/tailwind.css'
 import 'pliny/search/algolia.css'
 
-import { Space_Grotesk } from 'next/font/google'
-import { SearchProvider, SearchConfig } from 'pliny/search'
+import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 import SectionContainer from '@/components/SectionContainer'
-import Footer from '@/components/Footer'
 import siteMetadata from '@/data/siteMetadata'
-import { ThemeProviders } from './theme-providers'
 import { Metadata } from 'next'
+import { Space_Grotesk } from 'next/font/google'
+import { SearchConfig, SearchProvider } from 'pliny/search'
+import { ThemeProviders } from './theme-providers'
 
 const space_grotesk = Space_Grotesk({
   subsets: ['latin'],
