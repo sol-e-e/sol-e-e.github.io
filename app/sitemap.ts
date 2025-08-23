806b1a2faf8a3d64440acb8bdfe7bd644b1a40a7
sol-e-e <iamsol.sollee@gmail.com>
2025-08-23 17:29:14 +0900

    🐛 add force-static

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,8 @@
-import { MetadataRoute } from 'next'
-import { allBlogs } from 'contentlayer/generated'
 import siteMetadata from '@/data/siteMetadata'
+import { allBlogs } from 'contentlayer/generated'
+import { MetadataRoute } from 'next'
+
+export const dynamic = 'force-static'
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const siteUrl = siteMetadata.siteUrl
